910005c4e0ed4e06a8cfffb2802298595dc48b4b
andrescheffer28 <anddrecosttascheffer@gmail.com>
2025-08-04 09:41:31 -0300

    add: tAtropelamento & tJogo

--- script/Respostas/AndreCostaScheffer/main.c
+++ script/Respostas/AndreCostaScheffer/main.c
@@ -28,6 +28,7 @@ typedef struct{
 
     int id;
     int centro_y;
+
     int qtdCarros;
     tCarro carros[10];
     int velocidade;
@@ -39,15 +40,45 @@ typedef struct{
 
 typedef struct{
 
+    int iteracao;
+    int pista_id;
+    int carro_id;
+    int galinha_x;
+    int galinha_y;
+
+}tAtropelamento;
+
+// funcoes tAtropelamento
+
+typedef struct{
+
     int largura;
     int altura;
+
     int qtdPistas;
     tPista pistas[13];
+
     char planoCartesiano[35][101];
 
 }tMapa;
 
 //funcoes tMapa
+
+typedef struct{
+
+    tGalinha galinha;
+    tMapa mapa;
+    int iteracao;
+    int ehAnimado;
+
+    int qtdAtropelamentos;
+    tAtropelamento atropelamentos[100];
+
+    int heatmap[35][100];
+
+}tJogo;
+
+//funcoes tJogo
 
 int main(){
 
